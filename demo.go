c0a06cd765f12acd1d3d835877ced9d9f327b276
Alisahhh <923431911@qq.com>
2020-03-07 21:45:45 +0800

    add some comments and fmt...

--- demo.go
+++ demo.go
@@ -2,81 +2,88 @@ package main
 
 import (
 	"database/sql"
-	"log"
- _ "github.com/go-sql-driver/mysql"
 	"fmt"
-	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"math"
 	"strconv"
 	"sync"
-	"math"
+	"time"
 )
 
-func getYAddr(xAddr int) int{
-	yDoubleAddr:= math.Sin(float64(xAddr) / 30 * (math.Pi))
-	return int(yDoubleAddr * 30 + 0.5) + 30
+func getYAddr(xAddr int) int {
+	yDoubleAddr := math.Sin(float64(xAddr) / 30 * (math.Pi))
+	return int(yDoubleAddr*30+0.5) + 30
 }
 
-func main(){
-
+func main() {
+	// Connect to the sql database
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:4000)/test?parseTime=true")
-    if err != nil{
-        log.Fatal(err)
-		}
-		fmt.Println("connection succedssed")
-		defer db.Close()
-		
-		orderPrefix := "CREATE TABLE IF NOT EXISTS test.testTable";
-		orderSuffix := "(id INT(4) PRIMARY KEY AUTO_INCREMENT NOT NULL, data varchar(50))"
-		var wg sync.WaitGroup
-		for i:=0;i <= 60;i ++{
-			order := orderPrefix + strconv.Itoa(i) + orderSuffix
-			wg.Add(1)
-			go func(){
-				_, err := db.Exec(order)
-				if err != nil{
-						log.Fatalln(err)
-				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connection succedssed")
+	defer db.Close()
 
-		t := time.NewTicker(time.Minute)
-		defer t.Stop()
+	// Create 61 tables named testTableX
+	orderPrefix := "CREATE TABLE IF NOT EXISTS test.testTable"
+	orderSuffix := "(id INT(4) PRIMARY KEY AUTO_INCREMENT NOT NULL, data varchar(50))"
+	var wg sync.WaitGroup
+	for i := 0; i <= 60; i++ {
+		order := orderPrefix + strconv.Itoa(i) + orderSuffix
+		wg.Add(1)
+		go func() {
+			_, err := db.Exec(order)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 
-		whichTime := 0;
-		yAddr := 30
-		dataValue := 0
+	// Set a 1 min time ticker
+	t := time.NewTicker(time.Minute)
+	defer t.Stop()
 
-		for whichTime <= 60{
-			select{
-				case <-t.C:{
-					dataValue = 0
-					yAddr = getYAddr(whichTime)
-					whichTime++
-					fmt.Println(yAddr)
-					fmt.Println(whichTime)
-					break
+	// Draw sin function
+	whichTime := 0
+	yAddr := 30
+	dataValue := 0
+
+	for whichTime <= 60 {
+		select {
+		case <-t.C:
+			{
+				// switch to next block
+				dataValue = 0
+				yAddr = getYAddr(whichTime)
+				whichTime++
+				fmt.Println("location (%d,%d)", whichTime, yAddr)
+				break
+			}
+		default:
+			{
+				// Insert
+				dataValue++
+				insertOrderPrefix := "INSERT INTO test.testTable"
+				insertOrderSuffix := "(data) VALUES (?)"
+				insertOrder := insertOrderPrefix + strconv.Itoa(yAddr) + " " + insertOrderSuffix
+				_, err := db.Exec(insertOrder, strconv.Itoa(dataValue))
+				if err != nil {
+					log.Fatalln(err)
 				}
-				default:{
-					dataValue ++
-					insertOrderPrefix := "INSERT INTO test.testTable"
-					insertOrderSuffix := "(data) VALUES (?)"
-					insertOrder := insertOrderPrefix +  strconv.Itoa(yAddr) + " " + insertOrderSuffix
-					_,err := db.Exec(insertOrder,strconv.Itoa(dataValue))
-					if err != nil{
-						log.Fatalln(err)
-					}
-					queryOrderPrefix := "SELECT * FROM test.testTable" 
-					queryOrder :=  queryOrderPrefix + strconv.Itoa(yAddr)
-					row, err := db.Query(queryOrder)
-					if err != nil{
-						log.Fatalln(err)
-					}
-					row.Close()
-					break
+
+				// Query
+				queryOrderPrefix := "SELECT * FROM test.testTable"
+				queryOrder := queryOrderPrefix + strconv.Itoa(yAddr)
+				row, err := db.Query(queryOrder)
+				if err != nil {
+					log.Fatalln(err)
 				}
+				row.Close()
+				break
 			}
 		}
-
+	}
 }
